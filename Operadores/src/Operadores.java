17ba88117fadaad00471111a47c006fb732f68bc
dapcjunior <danielpereiracamposjunior@gmail.com>
2023-02-19 18:26:48 -0300

    trilha java dio

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,22 @@
+public class Operadores
+{
+    public static void main(String[] args)
+    {
+
+        // CONCATENAÇÂO
+        String nomeCompleto = "Linguagem" + " " + "Java";
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        concatenacao  = 1 + "1" + 1 + "1";
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
+    }
+}
---
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -0,0 +1,23 @@
+public class TiposVariaveis
+{
+    public static void main(String[] args) throws Exception
+    {
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //  casting
+
+        int  numero = 5;
+
+        int numero = 10;
+
+        System.out.println(numero);
+
+        double VALOR_DE_PI = 3.14;
+
+        VALOR_DE_PI = 10.75;
+
+        System.out.println(VALOR_DE_PI);
+
+    }
+}
